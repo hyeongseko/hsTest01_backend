089b87dea45321b5954493962d9a2c0d61e2cd9d
hyeongseko <link3504@gmail.com>
2025-04-30 17:29:00 +0900

    Project 04.30

---
+++ src/main/java/com/shTest/entity/ComDetailCd.java
@@ -0,0 +1,30 @@
+package com.shTest.entity;
+
+import com.shTest.dto.CalendarDto;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "com_detail_cd")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ComDetailCd {
+
+    @Id
+    @Column(name = "COM_DETAIL_CD_ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String comDetailCdId;
+
+    @Column(name = "COM_CLSF_CD_ID")
+    private String comClsfCdId;
+
+    @Column(name = "COM_DETAIL_CD_NM")
+    private String ComDetailCdNm;
+
+    public ComDetailCd(CalendarDto calDto) {
+        this.comDetailCdId = calDto.getSchdlCd();
+        this.ComDetailCdNm = calDto.getComDetailCdNm();
+    }
+}
---
+++ src/main/java/com/shTest/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.shTest.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
+    }
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+}
